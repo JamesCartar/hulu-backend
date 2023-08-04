9b6146499028ec3fcdd12b4900a4833e492d8af0
Hein Htet <arjunyadav.hash@gmail.com>
2023-08-04 23:55:07 +0630

    removing comments in connectDB

--- config/connectDb.js
+++ config/connectDb.js
@@ -15,23 +15,3 @@ const connectDB = (url) => {
 
 module.exports = connectDB;
 
-/* 
-not working with createConnection because
-Mongoose lets you start using your models immediately, without waiting for mongoose to establish a connection to MongoDB.
-That’s because mongoose buffers model function calls internally. This buffering is convenient, but also a common source of confusion.
-Mongoose will not throw any errors by default if you use a model without connecting.
-*/
-
-// const connectDB = (url) => {
-//   const dbString = url;
-//     const dbOptions = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     }
-
-//     const connection = mongoose.createConnection(dbString, dbOptions);
-//     connection.on('connected', () => {
-//         console.log('connected successfully');
-//     });
-// };
-
