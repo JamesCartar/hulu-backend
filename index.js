021b1c6c033afa9ddf60ae0246ac78c8f013ee23
Hein Htet <arjunyadav.hash@gmail.com>
2023-08-05 15:19:02 +0630

    adding vercel frontend allow origin and localhost:3000

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://master--arjun-hulu-clone.netlify.app"
+    origin: ["https://hulu-frontend.vercel.app", "http://localhost:3000"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
